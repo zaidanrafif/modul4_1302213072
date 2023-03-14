3ddeef5a44406bebb569c19b9ca360adf47a704e
PC9-IFLAB <91200696+aishaptrn@users.noreply.github.com>
2023-03-14 16:41:06 +0700

    menambahkan kode buah
    menambahkan kode buah

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class KodeBuah
+{
+    public enum namaBuah { Apel, Aprikot, Alpukat, Pisang, Paprika, Blackberry, Ceri, Kelapa, Jagung, Kurma, Durian, Anggur, Melon, Semangka }
+
+    public static string getKodeBuah(namaBuah Buah)
+    {
+        string[] KodeBuah = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00", "K00", "L00", "M00", "N00", "O00", };
+        return KodeBuah[(int)Buah];
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Nama Buah" + KodeBuah.namaBuah.Pisang + " Kode Buah " + KodeBuah.getKodeBuah(KodeBuah.namaBuah.Pisang));
+    }
+}
